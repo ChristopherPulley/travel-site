1bb0284f7752cc120042462ba131f7536a2752a9
Christopher F Pulley <christopher.f.pulley@gmail.com>
2020-09-15 21:25:50 -0500

    lesson_17

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,1 @@
-alert("hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+import "../styles/styles.css";
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require("path"); // integrated node package
+const postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer"),
+];
 
 module.exports = {
   entry: "./app/assets/scripts/App.js", // starting file
@@ -8,4 +14,23 @@ module.exports = {
   },
   mode: "development",
   watch: true, // tell webpack to rebundle on save
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader?url=false",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: postCSSPlugins,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
