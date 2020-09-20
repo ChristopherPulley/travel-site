30aca2467c4cea9c9567522c62dc27eab3e5e232
Christopher F Pulley <christopher.f.pulley@gmail.com>
2020-09-20 18:19:34 -0500

    Added mixins 6.2.2

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,6 @@
 import "../styles/styles.css";
+
+if (module.hot) {
+  // allows js and css to update without reload
+  module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path"); // integrated node package
 const postCSSPlugins = [
   require("postcss-import"),
+  require("postcss-mixins"),
   require("postcss-simple-vars"),
   require("postcss-nested"),
   require("autoprefixer"),
@@ -12,8 +13,17 @@ module.exports = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app"), // where bundled.js goes
   },
+  devServer: {
+    // webpack dev server to all hot content update
+    before: (app, server) => {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  },
   mode: "development",
-  watch: true, // tell webpack to rebundle on save
   module: {
     rules: [
       {
